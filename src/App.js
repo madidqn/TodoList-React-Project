707034975ab7d3fcb20a23e52867ce23daee15e3
mahdieh <madidgn82@gmail.com>
2024-07-06 02:30:15 +0430

    start logic

--- src/App.js
+++ src/App.js
@@ -1,44 +1,57 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [task, setTask] = useState("");
+  const [todos, setTodos] = useState([]);
+  function submit(e) {
+    e.preventDefault();
+    console.log(task);
+    setTodos(task);
+    setTask("");
+  }
   return (
     <div className="app">
       <div>
         <h2>Todo List</h2>
-        <form>
+        <form onSubmit={(e) => submit(e)}>
           <div>
-            <input type="text" />
+            <input
+              type="text"
+              value={task}
+              onChange={(e) => setTask(e.target.value)}
+            />
             <input type="checkbox" />
           </div>
           <button>Add</button>
-          <button>Edit</button>
-          <ul>
-            <div>
-              <li>task</li>
-              <div>
-                <i class="bx bxs-trash"></i>
-                <i class="bx bxs-edit-alt"></i>
-                <i class="bx bx-select-multiple"></i>
-              </div>
-            </div>
-            <div>
-              <li>task</li>
-              <div>
-                <i class="bx bxs-trash"></i>
-                <i class="bx bxs-edit-alt"></i>
-                <i class="bx bx-select-multiple"></i>
-              </div>
-            </div>
-            <div>
-              <li>task</li>
-              <div>
-                <i class="bx bxs-trash"></i>
-                <i class="bx bxs-edit-alt"></i>
-                <i class="bx bx-select-multiple"></i>
-              </div>
-            </div>
-          </ul>
+          <button type="button">Edit</button>
         </form>
+        <ul>
+          <div>
+            <li>{todos}</li>
+            <div>
+              <i className="bx bxs-trash"></i>
+              <i className="bx bxs-edit-alt"></i>
+              <i className="bx bx-select-multiple"></i>
+            </div>
+          </div>
+          {/* <div>
+            <li>task</li>
+            <div>
+              <i className="bx bxs-trash"></i>
+              <i className="bx bxs-edit-alt"></i>
+              <i className="bx bx-select-multiple"></i>
+            </div>
+          </div>
+          <div>
+            <li>task</li>
+            <div>
+              <i className="bx bxs-trash"></i>
+              <i className="bx bxs-edit-alt"></i>
+              <i className="bx bx-select-multiple"></i>
+            </div>
+          </div> */}
+        </ul>
       </div>
       <div>
         <h2>Todo Done</h2>
@@ -46,17 +59,17 @@ function App() {
           <div>
             <li>task</li>
             <div>
-              <i class="bx bxs-trash"></i>
-              <i class="bx bxs-edit-alt"></i>
-              <i class="bx bx-redo bx-rotate-180"></i>
+              <i className="bx bxs-trash"></i>
+              <i className="bx bxs-edit-alt"></i>
+              <i className="bx bx-redo bx-rotate-180"></i>
             </div>
           </div>
           <div>
             <li>task</li>
             <div>
-              <i class="bx bxs-trash"></i>
-              <i class="bx bxs-edit-alt"></i>
-              <i class="bx bx-redo bx-rotate-180"></i>
+              <i className="bx bxs-trash"></i>
+              <i className="bx bxs-edit-alt"></i>
+              <i className="bx bx-redo bx-rotate-180"></i>
             </div>
           </div>
         </ul>
