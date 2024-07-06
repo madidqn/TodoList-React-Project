1e150f1da3942035423c456f00a53eec2584db53
mahdieh <madidgn82@gmail.com>
2024-07-06 20:36:44 +0430

    clean code

--- src/App.js
+++ src/App.js
@@ -11,11 +11,10 @@ function App() {
   const [todoDone, setTodoDone] = useState([]);
   const [idTask, setIdTask] = useState(0);
   // completed tasks//
-  function submit(e) {
-    e.preventDefault();
-    //post data
+  // post tasks
+  function postTasks(list) {
     try {
-      fetch(`${url}todo`, {
+      fetch(`${url}${list}`, {
         method: "POST",
         body: JSON.stringify({
           task: inputValue,
@@ -31,14 +30,45 @@ function App() {
       console.log(e);
     }
   }
-
-  // get data
-  async function getData() {
-    const response = await fetch(`${url}todo`);
-    const data = await response.json();
-    setTodos(data);
+  function submit(e) {
+    e.preventDefault();
+    if (checkBox) {
+      postTasks("todoDone");
+    } else {
+      postTasks("todo");
+    }
+    // try {
+    //   fetch(`${url}todo`, {
+    //     method: "POST",
+    //     body: JSON.stringify({
+    //       task: inputValue,
+    //       checked: checkBox,
+    //     }),
+    //     headers: {
+    //       "Content-type": "aplication/json",
+    //     },
+    //   });
+    //   setCheckBox(false);
+    //   setInputValue("");
+    // } catch (e) {
+    //   console.log(e);
+    // }
   }
-  getData();
+  // get tasks
+  async function getTasks(list) {
+    if (list === "todo") {
+      const response = await fetch(`${url}todo`);
+      const data = await response.json();
+      setTodos(data);
+    } else {
+      const response = await fetch(`${url}todoDone`);
+      const data = await response.json();
+      setTodoDone(data);
+    }
+  }
+  getTasks("todo");
+  getTasks("todoDone");
+
   // delete task
   function deleteTask(id) {
     let confirmation = window.confirm("Are you shure?");
@@ -48,7 +78,7 @@ function App() {
       });
     }
   }
-  // get task
+  // get single task
   function getTask(id) {
     setEdit(true);
     const filterData = todos.filter((user) => user.id === id);
@@ -74,32 +104,36 @@ function App() {
     setInputValue("");
   }
   // list done
+
   // postTask
   // "http://localhost:5000/todoDone"
-  function postTask(id) {
-    const filterData = todos.filter((user) => user.id === id);
-    deleteTask(id);
-    try {
-      fetch(`${url}todoDone`, {
-        method: "POST",
-        body: JSON.stringify({
-          task: filterData[0].task,
-          checked: filterData[0].checked,
-        }),
-        headers: {
-          "Content-type": "aplication/json",
-        },
-      });
-    } catch (e) {
-      console.log(e);
-    }
-  }
-  async function getDone() {
-    const response = await fetch(`${url}todoDone`);
-    const data = await response.json();
-    setTodoDone(data);
-  }
-  getDone();
+  // function submitTaskDone(id) {
+  //   const filterData = todos.filter((user) => user.id === id);
+  //   deleteTask(id);
+  //   try {
+  //     fetch(`${url}todoDone`, {
+  //       method: "POST",
+  //       body: JSON.stringify({
+  //         task: filterData[0].task,
+  //         checked: filterData[0].checked,
+  //       }),
+  //       headers: {
+  //         "Content-type": "aplication/json",
+  //       },
+  //     });
+  //   } catch (e) {
+  //     console.log(e);
+  //   }
+  // }
+  // async function getDone() {
+  //   const response = await fetch(`${url}todoDone`);
+  //   const data = await response.json();
+  //   setTodoDone(data);
+  // }
+  // getDone();
+  //
+
+  //delete taskDone
   function deleteTaskDone(id) {
     let confirmation = window.confirm("Are you shure?");
     if (confirmation) {
@@ -108,11 +142,19 @@ function App() {
       });
     }
   }
-  function postTaskDone(id) {
-    const filterData = todoDone.filter((user) => user.id === id);
-    deleteTaskDone(id);
+  function sendToUndoTasks(id, list) {
+    // const filterData = todoDone.filter((user) => user.id === id);
+    let filterData;
+    // deleteTaskDone(id);
+    if (list === "todoDone") {
+      filterData = todoDone.filter((user) => user.id === id);
+      deleteTaskDone(id);
+    } else {
+      filterData = todos.filter((user) => user.id === id);
+      deleteTask(id);
+    }
     try {
-      fetch(url, {
+      fetch(`${url}todo`, {
         method: "POST",
         body: JSON.stringify({
           task: filterData[0].task,
@@ -168,7 +210,7 @@ function App() {
                 ></i>
                 <i
                   className="bx bx-select-multiple"
-                  onClick={() => postTask(todo.id)}
+                  onClick={() => sendToUndoTasks(todo.id, "todoDone")}
                 ></i>
               </div>
             </div>
@@ -188,7 +230,7 @@ function App() {
                 ></i>
                 <i
                   className="bx bx-redo bx-rotate-180"
-                  onClick={() => postTaskDone(todo.id)}
+                  onClick={() => sendToUndoTasks(todo.id, "todo")}
                 ></i>
               </div>
             </div>
